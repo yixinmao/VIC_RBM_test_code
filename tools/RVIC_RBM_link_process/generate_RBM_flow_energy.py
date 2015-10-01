d513be5e8a4ccfae869b4d333aebd3cee318294c
ymao <ymao@hydro.washington.edu>
2015-10-01 12:58:09 -0700

    Completed script 'generate_RBM_flow_energy.Salmon.cfg' - now it generates both energy and flow file as input format to RBM

--- tools/RVIC_RBM_link_process/generate_RBM_flow_energy.py
+++ tools/RVIC_RBM_link_process/generate_RBM_flow_energy.py
@@ -17,8 +17,8 @@ cfg = my_functions.read_config(sys.argv[1])  # Read config file
 # [INPUT]
 # Routing station file - output from 'prepare_RBM_param.py'
 route_station_file = cfg['INPUT']['route_station_file']
-# VIC output nc file - flow
-vic_output_flow_nc = cfg['INPUT']['vic_output_flow_nc']
+# RVIC output nc file (must be "grid" format)
+RVIC_output_nc = cfg['INPUT']['RVIC_output_nc']
 # VIC output nc file - energy
 vic_output_energy_nc = cfg['INPUT']['vic_output_energy_nc']
 # VIC output time step - flow (units: hour)
@@ -26,13 +26,29 @@ vic_flow_dt = cfg['INPUT']['vic_flow_dt']
 vic_energy_dt = cfg['INPUT']['vic_energy_dt']
 
 # [RBM_OPTIONS]
-# RBM start and end date
+# RBM start and end date (dt.date)
 start_date = dt.date(cfg['RBM_OPTIONS']['start_date'][0], \
                          cfg['RBM_OPTIONS']['start_date'][1], \
                          cfg['RBM_OPTIONS']['start_date'][2])
 end_date = dt.date(cfg['RBM_OPTIONS']['end_date'][0], \
                          cfg['RBM_OPTIONS']['end_date'][1], \
                          cfg['RBM_OPTIONS']['end_date'][2])
+# Same, but dt.datetime
+start_datetime = dt.datetime(cfg['RBM_OPTIONS']['start_date'][0], \
+                         cfg['RBM_OPTIONS']['start_date'][1], \
+                         cfg['RBM_OPTIONS']['start_date'][2])
+end_datetime = dt.datetime(cfg['RBM_OPTIONS']['end_date'][0], \
+                         cfg['RBM_OPTIONS']['end_date'][1], \
+                         cfg['RBM_OPTIONS']['end_date'][2])
+
+# [HYDRAULIC_OPTIONS]
+# Leopold coefficients (constant throughout basin)
+# <a_d> and <b_d>: a and b coefficients in flow depth estimation: z=aQ^b, where Q is flow discharge [cfs], z is flow depth [ft]
+a_d = cfg['HYDRAULIC_OPTIONS']['a_d']
+b_d = cfg['HYDRAULIC_OPTIONS']['b_d']
+# <a_w> and <b_w>: a and b coefficients in channel width estimation: w=aQ^b, where Q is flow discharge [cfs], w is channel width [ft]
+a_w = cfg['HYDRAULIC_OPTIONS']['a_w']
+b_w = cfg['HYDRAULIC_OPTIONS']['b_w']
 
 #====================================================#
 # Load latlon list for flow and energy grid cells
@@ -45,7 +61,9 @@ n_flow = int(line.split()[0])
 n_energy = int(line.split()[1])
 #=== Loop over each flow/energy grid cell ==#
 list_flow_lat_lon = []  # list of flow cells ([lat, lon])
+list_flow_cell_number = []  # list of cell number of flow cells (corresponding to list_flow_lat_lon)
 list_energy_lat_lon = []  # list of energy cells ([lat, lon])
+cell_number = 1
 while 1:
     line = f.readline().rstrip("\n")
     if line=="":
@@ -53,19 +71,63 @@ while 1:
     if line.split()[0]=="1":  # if this is a flow cell (i.e., not the end of a reach)
         lat_lon = line.split()[2]
         list_flow_lat_lon.append(lat_lon)
+        list_flow_cell_number.append(cell_number)
         list_energy_lat_lon.append(lat_lon)
         line = f.readline().rstrip("\n")  # Read the next line - None or path for uh_s file
     elif line.split()[0]=="0":  # if this is NOT a flow cell (i.e., the end of a reach)
                                 # Only energy grid cell
         lat_lon = line.split()[2]
         list_energy_lat_lon.append(lat_lon)
+    cell_number = cell_number + 1
 #=== Check whether the number of flow/energy cells are correct ===#
 if len(list_flow_lat_lon)!=n_flow or len(list_energy_lat_lon)!=n_energy:
     print 'Error: incorrect number of flow/energy cells!'
     exit()
 
 #====================================================#
-# Load VIC output data - energy
+# Load and process VIC output data - flow
+#====================================================#
+print 'Loading and processing VIC output flow data...'
+
+#=== Load data ===#
+ds_flow = xray.open_dataset(RVIC_output_nc)
+da_flow = ds_flow['streamflow'][:-1,:,:]  # Remove the last junk time step
+
+#=== Put data for each grid cell into a df ===#
+list_df = []
+for i, lat_lon in enumerate(list_flow_lat_lon):
+    lat = float(lat_lon.split('_')[0])
+    lon = float(lat_lon.split('_')[1])
+    df_flow = pd.DataFrame(da_flow.loc[:,lat,lon].values, index=da_flow.coords['time'].values, columns=['flow'])
+    # Select time range
+    df_flow = my_functions.select_time_range(df_flow, start_datetime, end_datetime)
+    # Convert units
+    df_flow = df_flow * pow(1000.0/25.4/12, 3)  # convert m3/s to cfs
+    # Create df and fill in variables
+    df = pd.DataFrame(index=df_flow.index)
+    df['day'] = range(1, len(df)+1)  # day number (1,2,3,...)
+    df['cell'] = list_flow_cell_number[i]  # cell number
+    df['Q_in'] = df_flow['flow'].values  # inflow discharge [cfs]
+    df['Q_out'] = df_flow['flow'].values  # outflow discharge [cfs]
+    df['Q_diff'] = 0.0  # lateral flow [cfs]
+    df['depth'] = a_d * pow(df_flow['flow'].values, b_d)  # flow depth [ft]
+    df['width'] = a_w * pow(df_flow['flow'].values, b_w)  # flow width [ft]
+    df['u'] = df_flow['flow'].values / df['depth'].values / df['width']  # flow velocoty [ft/s]
+    list_df.append(df)
+
+#=== Rearrange data ===#
+# Combine df of all grid cells together, in a single multiindex df (indice: cell; date)
+df_flow = pd.concat(list_df, keys=list_flow_cell_number)
+
+#=== Switch order of indice (to: date; cell), then sort ===#
+df_flow = df_flow.reorder_levels([1,0], axis=0)
+df_flow = df_flow.sortlevel(0)
+
+#=== Close dataset ===#
+ds_flow.close()
+
+#====================================================#
+# Load and process VIC output data - energy
 #====================================================#
 print 'Loading and processing VIC output energy data...'
 
@@ -77,13 +139,8 @@ ds_vic_energy = xray.open_dataset(vic_output_energy_nc)
 #=== If sub-daily data, convert to daily ===#
 print 'Converting sub-daily to daily data...'
 for var in ['Tair', 'vp', 'Shortwave', 'Longwave', 'Density', 'Pressure', 'Wind']:
-    if vic_energy_dt<24:
-        vic_energy_daily[var] = ds_vic_energy[cfg['INPUT'][var]]\
+    vic_energy_daily[var] = ds_vic_energy[cfg['INPUT'][var]]\
                                 .groupby('time.date').mean(dim='time')
-    elif vic_energy_dt==24:
-        vic_energy_daily[var] = ds_vic_energy[cfg['INPUT'][var]]
-    else:
-         print 'Error: VIC energy output time interval must be 24 or less hours!'
 
 #=== Select time range for RBM ===#
 print 'Selecting time range...'
@@ -99,8 +156,11 @@ vic_energy_daily['Longwave'] = vic_energy_daily['Longwave'] \
                                * 2.388 * pow(10, -4) # convert [W/m2] to [mm*K/s]
 vic_energy_daily['Pressure'] = vic_energy_daily['Pressure'] * 10.0 # convert [kPa] to [mb]
 
+#=== Close dataset ===#
+ds_vic_energy.close()
+
 #====================================================#
-# Rearrange data
+# Rearrange data - energy
 #====================================================#
 #=== Put data for each grid cell into a df ===#
 list_df = []
@@ -131,11 +191,5 @@ df_energy = df_energy.sortlevel(0)
 #====================================================#
 # Write data to file
 #====================================================#
+np.savetxt(cfg['OUTPUT']['rbm_flow_file'], df_flow.values, fmt='%d %d %.1f %.1f %.1f %.1f %.1f %.2f')
 np.savetxt(cfg['OUTPUT']['rbm_energy_file'], df_energy.values, fmt='%d %.1f %.1f %.4f %.4f %.3f %.1f %.1f')
-
-
-
-
-
-
-
